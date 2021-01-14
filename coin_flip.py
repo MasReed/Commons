f06f59fa215fdaba1dc1fe1446ef28afdb08136f
masreed <bluekitdrummer@gmail.com>
2021-01-13 23:28:52 -0600

    implemented simple visualization

--- coin_flip.py
+++ coin_flip.py
@@ -19,10 +19,9 @@ def flip(n: int) -> dict:
 
     # determine heads or tails for each flip of `n` flips.
     for i in range(int(n)):
-        rand = random.random()
 
         # P(heads) = P(tails) = .5
-        if rand <= 0.5:
+        if random.random() <= 0.5:
             counts['heads'] += 1
         else:
             counts['tails'] += 1
@@ -30,7 +29,29 @@ def flip(n: int) -> dict:
     return counts
 
 
+def visualize(counts: dict) -> None:
+    """Displays a simple visual representation of the relative dictionary values.
+
+    Args:
+        counts (dict): Dictionary containing integers as values for the keys
+        'heads' and 'tails'.
+
+    Returns:
+        None: Prints visualization to screen.
+    """
+    BAR_NUM = 50  # Affects display, larger takes up more screen space on output
+
+    heads_count = counts['heads']
+    tails_count = counts['tails']
+    total_count = heads_count + tails_count
+
+    # print normalized value of each key
+    for key, value in counts.items():
+        bars = (value / total_count)*BAR_NUM
+        print(key+':', '\u258A'*round(bars), value)
+
+
 if __name__ == '__main__':
     while True:
-        user_flips = input('How many coinflips? ')
-        print(flip(user_flips))
+        user_flips = input('\nHow many coinflips? ')
+        visualize(flip(user_flips))
