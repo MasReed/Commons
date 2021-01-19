50d139e80151112b472e46ed4d2ab30e154001e2
masreed <bluekitdrummer@gmail.com>
2021-01-19 14:00:12 -0600

    Added comments

--- algorithmic/bubble_sort.py
+++ algorithmic/bubble_sort.py
@@ -1,9 +1,17 @@
 
 def bubble_sort(lst: list) -> list:
-    """
+    """Sorts a numerical list in ascending order by comparing two numbers at a
+    time, then moving the larger one further along the list.
+
+    Args:
+        lst (list): The list to be sorted.
+
+    Returns:
+        list: The list sorted in ascending order.
     """
 
     for i in range(len(lst)):
+        # last two elements to compare are n-2 and n-1
         for j in range(len(lst) - 1):
             if lst[j] > lst[j+1]:
                 lst[j], lst[j+1] = lst[j+1], lst[j]
